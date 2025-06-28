e6ae6d08e6c090d896b17ad8fb2a3f3572402b3c
abdallah999-rgb <mah.abdallah999@gmail.com>
2025-06-28 14:58:00 +0300

    - refactor category item

---
+++ lib/l10n/app_localizations_ar.dart
@@ -0,0 +1,62 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get home => 'الرئيسية';
+
+  @override
+  String get goodMorningHereIsSomeNewsForYou =>
+      '          صباح الخير          \n        إليك آخر الأخبار';
+
+  @override
+  String get general => 'عام';
+
+  @override
+  String get business => 'اقتصاد';
+
+  @override
+  String get entertainment => 'ترفيه';
+
+  @override
+  String get sports => 'رياضة';
+
+  @override
+  String get health => 'صحة';
+
+  @override
+  String get technology => 'تكنولوجيا';
+
+  @override
+  String get viewFullArticle => 'عرض الخبر كاملًا';
+
+  @override
+  String get search => 'بحث';
+
+  @override
+  String get goToHome => 'العودة للرئيسية';
+
+  @override
+  String get theme => 'السمة';
+
+  @override
+  String get dark => 'داكن';
+
+  @override
+  String get light => 'فاتح';
+
+  @override
+  String get language => 'اللغة';
+
+  @override
+  String get notFoundAnyArticles => 'عذرًا، لا توجد مقالات لعرضها';
+
+  @override
+  String get science => 'علوم';
+}
---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,62 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get home => 'Home';
+
+  @override
+  String get goodMorningHereIsSomeNewsForYou =>
+      'Good Morning\nHere is Some News For You';
+
+  @override
+  String get general => 'General';
+
+  @override
+  String get business => 'Business';
+
+  @override
+  String get entertainment => 'Entertainment';
+
+  @override
+  String get sports => 'Sports';
+
+  @override
+  String get health => 'Health';
+
+  @override
+  String get technology => 'Technology';
+
+  @override
+  String get viewFullArticle => 'View Full Article';
+
+  @override
+  String get search => 'Search';
+
+  @override
+  String get goToHome => 'Go To Home';
+
+  @override
+  String get theme => 'Theme';
+
+  @override
+  String get dark => 'Dark';
+
+  @override
+  String get light => 'Light';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get notFoundAnyArticles => 'Not Found Any Articles';
+
+  @override
+  String get science => 'Science';
+}
